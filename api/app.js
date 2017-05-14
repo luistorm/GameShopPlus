e7c603147cfc9fd2ffd53a6027de8b51434ae30c
gabrielcolmena <gbrieldcg@gmail.com>
2017-05-13 21:23:17 -0400

    Chat app added to api

--- api/app.js
+++ api/app.js
@@ -1,21 +1,18 @@
 'use strict';
 
-const express 	= require('express'),
- 	  server 	= require('http').Server(express),
-	  io 		= require('socket.io')(server),
-	  Client	= require('mariasql'),
-	  cors 		= require('cors'),
+const 	express = require('express'),
+ 		app     = express(),
+ 		server  = require('http').createServer(app),
+		io      = require('socket.io').listen(server),
+	 	Client	= require('mariasql'),
+	  	cors 	= require('cors'),
 
-
-	//Global 'system' variables
-	__server_port = 3000,
-	__server_ip_address = '127.0.0.1';
-
-//Server object
-const app = express(),
+		//Global 'system' variables
+		__server_port = 3000,
+		__server_ip_address = '127.0.0.1';
 
 //DB Client object
-	dbClient = new Client({
+const	dbClient = new Client({
 		host: __server_ip_address,
 		user: 'root',
 		password: '',
@@ -33,26 +30,30 @@ app.use(cors());
 app.use('/public', express.static('public'));
 
 //Unique get endpoint
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.render('index.html');
 });
 
-//socket.io default code
+
+var users = 0;
 io.on('connection', (socket) => {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', (data) => {
-		console.log(data);
+	users++;
+	io.emit('users', users);
+
+	socket.on('incommingMessage', (res) => {
+		io.emit('outcommingMessage', res);
+	});
+
+	socket.on('disconnect', () => {
+		users--;
+		io.emit('disconnect', users);
 	});
 });
 
-
-app.listen(__server_port, __server_ip_address, () => {
-	console.log('\x1b[37m','API listen on:');
-	console.log('\x1b[36m', __server_ip_address);
-	console.log('\x1b[37m','Port:');
-	console.log('\x1b[36m', __server_port);
-	console.log('\x1b[37m','DBClient connected at:');
-	console.log('\x1b[36m', __server_ip_address);
-	console.log('\x1b[37m','Database name:');
-	console.log('\x1b[36m', dbClient._config.db);
+server.listen(__server_port, __server_ip_address, () => {
+	console.log('\x1b[37m','API listen on:', '\x1b[36m', __server_ip_address);
+	console.log('\x1b[37m','Port:', '\x1b[36m', __server_port);
+	console.log('\x1b[37m','DBClient connected at:', '\x1b[36m', __server_ip_address);
+	console.log('\x1b[37m','Database name:', '\x1b[36m', dbClient._config.db);
+	console.log('\x1b[37m','-------------------------------------');
 });
